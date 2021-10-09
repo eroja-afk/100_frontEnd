89a6d039a5ff32082c092b7a7c805bee9e989939
eroja-afk <eroja.afk@gmail.com>
2021-10-10 01:25:27 +0800

    with units view

--- splash2.php
+++ splash2.php
@@ -16,15 +16,33 @@
         <script type="text/javascript" charset="utf8" src="datatables.js"></script>
         <script type="text/javascript" charset="utf8" src="datatables.min.js"></script>
         
+         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+    crossorigin=""/>
+  <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+    integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+    crossorigin=""></script>
+
+  <!-- Load Esri Leaflet from CDN -->
+  <script src="https://unpkg.com/esri-leaflet@2.5.0/dist/esri-leaflet.js"
+    integrity="sha512-ucw7Grpc+iEQZa711gcjgMBnmd9qju1CICsRaryvX7HJklK0pGl/prxKvtHwpgm5ZHdvAil7YPxI1oWPOWK3UQ=="
+    crossorigin=""></script>
+
+  <!-- Load Esri Leaflet Geocoder from CDN -->
+  <link rel="stylesheet" href="https://unpkg.com/esri-leaflet-geocoder@2.3.3/dist/esri-leaflet-geocoder.css"
+    integrity="sha512-IM3Hs+feyi40yZhDH6kV8vQMg4Fh20s9OzInIIAc4nx7aMYMfo+IenRUekoYsHZqGkREUgx0VvlEsgm7nCDW9g=="
+    crossorigin="">
+  <script src="https://unpkg.com/esri-leaflet-geocoder@2.3.3/dist/esri-leaflet-geocoder.js"
+    integrity="sha512-HrFUyCEtIpxZloTgEKKMq4RFYhxjJkCiF5sDxuAokklOeZ68U2NPfh4MFtyIVWlsKtVbK5GD2/JzFyAfvT5ejA=="
+    crossorigin=""></script>
         <title>
             Stat Map
         </title>
     </head>
     <body>
-    	
-    	<nav class="navbar navbar-expand-xl navbar-light bg-light">
+    	<nav class="navbar navbar-expand-xl navbar-dark bg-primary">
   			<div class="container-fluid">
-    			<a class="navbar-brand" href="#" style="font-weight: bold" >100</a>
+    			<a class="navbar-brand" href="#" style="font-weight: bold" >RECAS</a>
     			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       				<span class="navbar-toggler-icon"></span>
     			</button>
@@ -40,11 +58,12 @@
     			</div>
   			</div>
 		</nav>
+        <div class = "container">
 		<div>
             <form class="content">
            		<h4><b><i>Longitude and Latitude:</i></b></h4>
-                <input id="lo" class="lo" type="text" name="longitude"placeholder="Longitude">
-                <input id="la" class="la" type="text" name="latitude" placeholder="Latitude">
+                <input id="lo" class="lo" type="text" name="longitude"placeholder="Longitude" disabled="">
+                <input id="la" class="la" type="text" name="latitude" placeholder="Latitude" disabled="">
                 <h4><b><i>Reporters Name:</i></b></h4>
                 <input id="name" type="text" name="name" placeholder="Enter Name">
                 <h4><b><i>Reporters Contact:</i></b></h4>
@@ -99,53 +118,40 @@
                 </tbody>
             </table>
         
-        
+        </div>
     </body>
 
     </html>
 
 <script>
 
-    var map = L.map('mapid').setView([10.3157, 123.8854], 14);
+var map = L.map('mapid').setView([10.3157, 123.8854], 14);
     var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         });
     osm.addTo(map);
-     
-
-    if(!navigator.geolocation){
-        console.log("Your browser doesn't support geolocation feature!");
-    }else{
-        setInterval(()=> {
-            navigator.geolocation.getCurrentPosition(getPosition);
-        }, 5000);
         
-    }
+  var geocodeService = L.esri.Geocoding.geocodeService();
+  var longitude;
+  var latitude;
 
-    var marker, circle;
+  map.on('click', function (e) {
+    geocodeService.reverse().latlng(e.latlng).run(function (error, result) {
+      if (error) {
+        return;
+      }
+      var test = result.latlng;
+      latitude = test.lat;
+      longitude = test.lng;
+      console.log(test,latitude,longitude);
 
-    function getPosition(position){
-        var lat = position.coords.latitude;
-        var long = position.coords.longitude;
-        var accuracy = position.coords.accuracy;
+      alert(result.latlng)
+      L.marker(result.latlng).addTo(map).bindPopup(result.address.Match_addr).openPopup();
+    });
+  });
 
-        if(marker) {
-            map.removeLayer(marker);
-        }
-
-        if(circle){
-            map.removeLayer(circle);
-        }
-
-        marker = L.marker([lat, long]);
-        circle = L.circle([lat, long], {radius: accuracy});
-
-        var featureGroup = L.featureGroup([marker, circle]).addTo(map);
-
-        map.fitBounds(featureGroup.getBounds());
-
-        console.log("Your coordinate is: Lat:"+ lat +" Long: "+ long + " Accuracy: "+ accuracy);
-    }
+ 
+  
 
     $("#fhuman").hide();
     $("#fprop").hide();
---
+++ units.php
@@ -0,0 +1,87 @@
+<html>
+    <head>
+    	<link rel="stylesheet" type="text/css" href="bootstrap-5.1.2-dist/css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="bootstrap-5.1.2-dist/css/bootstrap.min.css">
+        <style>
+        #mapid {
+            height: 100%;
+            width: 100%;
+        }
+        </style>
+    	<script type="text/javascript" charset="utf8" src="bootstrap-5.1.2-dist/js/bootstrap.js"></script>
+        <script type="text/javascript" charset="utf8" src="bootstrap-5.1.2-dist/js/bootstrap.min.js"></script>
+        <script src="./resources/leaflet/leaflet.js"></script>
+        <script src="./resources/jquery-3.6.0.min.js"></script>
+
+
+        <link rel="stylesheet" href="./resources/leaflet/leaflet.css">
+        <link rel="stylesheet" href="navbar.css">
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+    crossorigin=""/>
+  <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+    integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+    crossorigin=""></script>
+
+  <!-- Load Esri Leaflet from CDN -->
+  <script src="https://unpkg.com/esri-leaflet@2.5.0/dist/esri-leaflet.js"
+    integrity="sha512-ucw7Grpc+iEQZa711gcjgMBnmd9qju1CICsRaryvX7HJklK0pGl/prxKvtHwpgm5ZHdvAil7YPxI1oWPOWK3UQ=="
+    crossorigin=""></script>
+
+  <!-- Load Esri Leaflet Geocoder from CDN -->
+  <link rel="stylesheet" href="https://unpkg.com/esri-leaflet-geocoder@2.3.3/dist/esri-leaflet-geocoder.css"
+    integrity="sha512-IM3Hs+feyi40yZhDH6kV8vQMg4Fh20s9OzInIIAc4nx7aMYMfo+IenRUekoYsHZqGkREUgx0VvlEsgm7nCDW9g=="
+    crossorigin="">
+  <script src="https://unpkg.com/esri-leaflet-geocoder@2.3.3/dist/esri-leaflet-geocoder.js"
+    integrity="sha512-HrFUyCEtIpxZloTgEKKMq4RFYhxjJkCiF5sDxuAokklOeZ68U2NPfh4MFtyIVWlsKtVbK5GD2/JzFyAfvT5ejA=="
+    crossorigin=""></script>
+        <title>
+            Stat Map
+        </title>
+    </head>
+    <body>
+    <nav class="navbar navbar-expand-xl navbar-dark bg-primary">
+  			<div class="container-fluid">
+    			<a class="navbar-brand" href="#" style="font-weight: bold" >RECAS</a>
+    			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      				<span class="navbar-toggler-icon"></span>
+    			</button>
+    			<div class="collapse navbar-collapse" id="navbarNav">
+      				<ul class="navbar-nav">
+        				<li class="nav-item">
+          					<a class="nav-link active" aria-current="page" href="#">Unit's View</a>
+        				</li>
+      				</ul>
+    			</div>
+  			</div>
+		</nav>
+        <div id="mapid"></div>
+    </body>
+</html>
+
+<script>
+
+var map = L.map('mapid').setView([10.3157, 123.8854], 14);
+    var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        });
+    osm.addTo(map);
+        
+  var geocodeService = L.esri.Geocoding.geocodeService();
+  var longitude;
+  var latitude;
+
+  map.on('click', function (e) {
+    geocodeService.reverse().latlng(e.latlng).run(function (error, result) {
+      if (error) {
+        return;
+      }
+      var test = result.latlng;
+      latitude = test.lat;
+      longitude = test.lng;
+      console.log(test,latitude,longitude);
+
+      L.marker(result.latlng).addTo(map).bindPopup(result.address.Match_addr).openPopup();
+    });
+  });
+</script>

910adaa2b5972f1003157210021d468ca6d79990
kent.setenta <kent.setenta@ewave.com>
2021-10-10 02:04:12 +0800

    login

--- login.php
+++ login.php
@@ -1,3 +1,5 @@
+<?php header('Access-Control-Allow-Origin: *'); ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,14 +9,13 @@
    	<script type="text/javascript" charset="utf8" src="bootstrap-5.1.2-dist/js/bootstrap.js"></script>
     <script type="text/javascript" charset="utf8" src="bootstrap-5.1.2-dist/js/bootstrap.min.js"></script>
     <script src="./resources/jquery-3.6.0.min.js"></script>
-	<title>Log In Sesh</title>
 </head>
 <body>
 	<div class="wrapper fadeInDown">
  		 <div id="formContent">
         <h1>RECAS-Login</h1>
 
-    <form>
+    <form id="loginForm">
       <input type="text" id="user" class="fadeIn second" name="username" placeholder="Enter User">
       <input type="password" id="password" class="fadeIn third" name="password" placeholder="Enter Password">
       <input type="submit" class="fadeIn fourth" value="Log In">
@@ -29,3 +30,32 @@
 </div>
 </body>
 </html>
+<script>
+$( document ).ready(function() {
+  
+  $('#loginForm').on('submit', function(e){
+    e.preventDefault();
+    const data = new FormData(e.target);
+    console.log("Datas - " + data.get('username'));
+
+    $.ajax({
+      url: 'https://recas-api.vercel.app/login',
+      type: 'post',
+      data: {
+        username: data.get('username'),
+        password: data.get('password')
+      },
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      dataType: 'json',
+        success: function(res){
+        console.log(res);
+        alert("Login");
+        $(location).attr('href','units.php');
+      }
+    });
+  });
+
+});
+</script>

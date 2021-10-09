bc2540c28ac92bd7c3c974a173b93ba0343f9b37
eroja-afk <eroja.afk@gmail.com>
2021-10-10 01:34:54 +0800

    with login and unit view

---
+++ login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="login.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap-5.1.2-dist/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="bootstrap-5.1.2-dist/css/bootstrap.min.css">
+   	<script type="text/javascript" charset="utf8" src="bootstrap-5.1.2-dist/js/bootstrap.js"></script>
+    <script type="text/javascript" charset="utf8" src="bootstrap-5.1.2-dist/js/bootstrap.min.js"></script>
+    <script src="./resources/jquery-3.6.0.min.js"></script>
+	<title>Log In Sesh</title>
+</head>
+<body>
+	<div class="wrapper fadeInDown">
+ 		 <div id="formContent">
+        <h1>RECAS-Login</h1>
+
+    <form>
+      <input type="text" id="user" class="fadeIn second" name="username" placeholder="Enter User">
+      <input type="password" id="password" class="fadeIn third" name="password" placeholder="Enter Password">
+      <input type="submit" class="fadeIn fourth" value="Log In">
+    </form>
+
+    <!-- Forget Password -->
+    <!-- <div id="formFooter">
+      <a class="underlineHover" href="#">Forgot Password?</a>
+    </div> -->
+
+  </div>
+</div>
+</body>
+</html>

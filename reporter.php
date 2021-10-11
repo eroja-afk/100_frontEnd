7903dd5b2fe699aca8577c47913b4e6340bb5185
unknown <josephjunejoe@yahoo.com>
2021-10-11 17:41:12 +0800

    asdasdasd

--- reporter.php
+++ reporter.php
@@ -1,13 +1,11 @@
 <html>
     <head>
+        <script src="./resources/jquery-3.6.0.min.js"></script>
     	<link rel="stylesheet" type="text/css" href="bootstrap-5.1.2-dist/css/bootstrap.css">
         <link rel="stylesheet" type="text/css" href="bootstrap-5.1.2-dist/css/bootstrap.min.css">
     	<script type="text/javascript" charset="utf8" src="bootstrap-5.1.2-dist/js/bootstrap.js"></script>
         <script type="text/javascript" charset="utf8" src="bootstrap-5.1.2-dist/js/bootstrap.min.js"></script>
         <script src="./resources/leaflet/leaflet.js"></script>
-        <script src="./resources/jquery-3.6.0.min.js"></script>
-
-
         <link rel="stylesheet" href="./resources/leaflet/leaflet.css">
         <link rel="stylesheet" href="splash.css">
         <link rel="stylesheet" href="navbar.css">
@@ -15,14 +13,12 @@
         <link rel="stylesheet" type="text/css" href="DataTables\datatables.min.css">
         <script type="text/javascript" charset="utf8" src="DataTables\datatables.js"></script>
         <script type="text/javascript" charset="utf8" src="DataTables\datatables.min.js"></script>
-        
-         <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
-    integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
-    crossorigin=""/>
-  <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
-    integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
-    crossorigin=""></script>
 
+        <link rel="stylesheet" type="text/css" href="jquery-ui\jquery-ui.css">
+        <script type="text/javascript" charset="utf8" src="jquery-ui\jquery-ui.js"></script>
+
+        
+        
   <!-- Load Esri Leaflet from CDN -->
   <script src="https://unpkg.com/esri-leaflet@2.5.0/dist/esri-leaflet.js"
     integrity="sha512-ucw7Grpc+iEQZa711gcjgMBnmd9qju1CICsRaryvX7HJklK0pGl/prxKvtHwpgm5ZHdvAil7YPxI1oWPOWK3UQ=="
@@ -113,6 +109,10 @@
                                     <option value="7">Carnapping</option>
                                 </select>
                             </div>
+                            <div class="mb-3">
+                                <label for="contact" class="form-label">Crime Barangay</label>
+                                <input class="form-control" id="barangay" type="text" name="contact" placeholder="Enter Barangay">
+                            </div>
                             <button class="btn btn-success form-control" id="submitCrime">Report Crime</button>
                     </div>
                 </div>
@@ -126,6 +126,46 @@
                     </div>
                 </div>
             </div>
+            <div>
+                <div class="mb-3">
+                    <label for="searchDetails" class="form-label">Crimes Against</label>
+                    <select class="form-control" id="searchchoice" >
+                    <option value="null">Select Option</option>
+                    <option value="0">Against Human</option>
+                    <option value="1">Against Property</option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <select class="form-control" id="searchcase" >
+                        <option value="null">Select Option</option>
+                        <option value="1">-Murder</option>
+                        <option value="2">-Homicide</option>
+                        <option value="3">-Physical Injuries</option>
+                        <option value="4">-Rape</option>
+                        <option value="5">Robbery</option>
+                        <option value="6">Theft</option>
+                        <option value="7">Carnapping</option>
+                    </select>
+                </div>
+                   <select class="form-control" id="searchstatus" >
+                        <option value="null">Select Option</option>
+                        <option value="1">Ongoing</option>
+                        <option value="2">Finished</option>
+                    </select>
+                    <select class="form-control" id="searchbarangay">
+                        <option value="null">Select Option</option>
+                        <option value="1">brgy1</option>
+                        <option value="2">brgy2</option>
+                        <option value="3">brgy3</option>
+                        <option value="4">brgy4</option>
+                    </select>          
+                    <label class="form-label">Reporter Contact</label>
+                    <input class="form-control" id="searchcontact" type="text" placeholder="Reporter Contact"></input>
+                    <p>From: <input type="text" id="searchfrom" class="datepicker" ></p>
+                    <p>To: <input type="text" id="searchto" class="datepicker" ></p>
+                    <input type="button" id="filterbtn" value="Filter"></input>
+                    
+            </div>
                 <table id="table_id" class="display">
                 <thead>
                     <tr>
@@ -137,6 +177,7 @@
                         <th>Reporter Address</th>
                         <th>latitude</th>
                         <th>longitude</th>
+                        <th>Status</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -173,8 +214,8 @@ $( document ).ready(function() {
     var markersLayer = new L.LayerGroup();
 
     var map = L.map('mapid').setView([10.3157, 123.8854], 14);
+    $('.datepicker').datepicker();
     
-
 
     var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -185,6 +226,7 @@ $( document ).ready(function() {
     var longitude;
     var latitude;
     var marker = null;
+
 
     map.on('click', function (e) {
         if(marker == null){
@@ -211,7 +253,9 @@ $( document ).ready(function() {
         }
     });
 
-    
+    $("#filterbtn").click(function(){
+        filterCrime();
+    })
 
     $("#rmvmarker").click(function(){
         deleteMarker();
@@ -258,7 +302,7 @@ $( document ).ready(function() {
 
             tempData.latitude = filteredRows.data()[i][6];
             tempData.longitude = filteredRows.data()[i][7];
-            makeMarker(tempData)
+            makeMarker(tempData,markersLayer)
         }
         
     }
@@ -270,15 +314,15 @@ $( document ).ready(function() {
         marker= null;
     }
 
-    function makeMarker(data){
-        var tempMarker = L.marker([data.latitude,data.longitude]).addTo(markersLayer).bindPopup(data);
-        markersLayer.addTo(map); 
+    function makeMarker(data,usedLayer){
+        var tempMarker = L.marker([data.latitude,data.longitude]).addTo(usedLayer).bindPopup(data);
+        usedLayer.addTo(map); 
     }
 
     
 
     function addTableRow(data){
-         table.row.add([data.against,data.type,data.datetime,data.reporter_name,data.reporter_contact,data.reporter_address,data.latitude,data.longitude]).draw(false);
+         table.row.add([data.against,data.type,data.date,data.reporter_name,data.reporter_contact,data.reporter_address,data.latitude,data.longitude,data.status]).draw(false);
     }
 
     
@@ -301,7 +345,7 @@ $( document ).ready(function() {
                     //console.log(tableData.data[i])
                     something = tableData.data[i];
                     dataAll.push(tableData.data[i]);
-                    makeMarker(tableData.data[i]);
+                    makeMarker(tableData.data[i],markersLayer);
                     addTableRow(something);
 
 
@@ -333,7 +377,8 @@ $( document ).ready(function() {
                 'report_details'     : $('#details').val(),
                 'latitude'     : $('#la').val(),
                 'longitude'     : $('#lo').val(),
-                'crimeType_id'     :ctype
+                'crimeType_id'     :ctype,
+                'barangay' :        $("#barangay").val()
             };
 
             
@@ -345,6 +390,35 @@ $( document ).ready(function() {
             dataType  : 'json',
             success   : function(data) {
                 alert("Crime reported")
+                
+            }
+            
+        });
+    }
+
+    function filterCrime(){
+        var contact ='null';
+        console.log(contact)
+        
+
+        var postForm = { //Fetch form data
+                'choice'     :          $("#searchchoice").val(),
+                'crimecase'     :       $("#searchcase").val(),
+                'status'     :          $("#searchstatus").val(),
+                'searchbarangay'     :  $("#searchbarangay").val(),
+                'contact'     :         contact,
+                'from'     :            $("#searchfrom").val(),
+                'to'     :              $("#searchto").val()
+            };
+
+        $.ajax({ //Process the form using $.ajax()
+            method      : 'POST', //Method type
+            url       : 'https://recas-api.vercel.app/searchCrime',
+            //url       : 'localhost:3000/reportCrime', //Your form processing file URL
+            data      : postForm, //Forms name
+            dataType  : 'json',
+            success   : function(data) {
+                console.log(data)
                 
             }
             
